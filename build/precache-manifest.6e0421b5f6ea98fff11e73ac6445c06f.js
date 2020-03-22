cd74674d6524160932e1084c8ee64f6b0edbf220
jperazas208 <jperaza@mayoreoexp.com>
2020-03-21 22:24:20 -0600

    Cambios para git page

---
+++ build/precache-manifest.6e0421b5f6ea98fff11e73ac6445c06f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7be0dc89f64845caa83cdd70a0728a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd1990ae635854c23c3",
+    "url": "/static/css/4.938c68d3.chunk.css"
+  },
+  {
+    "revision": "85ee37f14b80b987b6c8",
+    "url": "/static/css/5.5b7d99f3.chunk.css"
+  },
+  {
+    "revision": "33a29935417cdc50ac5b",
+    "url": "/static/css/main.4048f8c2.chunk.css"
+  },
+  {
+    "revision": "4e13bdbd9bf997cefc4e",
+    "url": "/static/js/0.087b3ca9.chunk.js"
+  },
+  {
+    "revision": "7ec09aa2a8d6739c12a35d6ee7dff5f7",
+    "url": "/static/js/0.087b3ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257012d59be25693b177",
+    "url": "/static/js/3.fff26cb5.chunk.js"
+  },
+  {
+    "revision": "b33b804d567154ef2f786de8dbaf049b",
+    "url": "/static/js/3.fff26cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd1990ae635854c23c3",
+    "url": "/static/js/4.e36fb643.chunk.js"
+  },
+  {
+    "revision": "85ee37f14b80b987b6c8",
+    "url": "/static/js/5.7a5a5f1c.chunk.js"
+  },
+  {
+    "revision": "91467a445f05da18ff51",
+    "url": "/static/js/6.bfe52ff2.chunk.js"
+  },
+  {
+    "revision": "a453f78b850f6681ed6c",
+    "url": "/static/js/7.8d3db19b.chunk.js"
+  },
+  {
+    "revision": "8cbc5a9960e8e6a59a06",
+    "url": "/static/js/8.8ee7253a.chunk.js"
+  },
+  {
+    "revision": "33a29935417cdc50ac5b",
+    "url": "/static/js/main.0b39a9c7.chunk.js"
+  },
+  {
+    "revision": "3e545e709665b6756168",
+    "url": "/static/js/runtime-main.a06eddbe.js"
+  },
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/static/media/feather.3d2fa2e5.woff"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/static/media/feather.6a750f01.ttf"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/static/media/feather.931c9e74.eot"
+  },
+  {
+    "revision": "b23df4db71a29dbb733a0e555a7db8f9",
+    "url": "/static/media/feather.b23df4db.svg"
+  },
+  {
+    "revision": "222e25f53e8f2a25ab93bb09f2b99e07",
+    "url": "/static/media/google.222e25f5.svg"
+  },
+  {
+    "revision": "fd58a052054c690506118c4b7a0f731d",
+    "url": "/static/media/login.fd58a052.png"
+  },
+  {
+    "revision": "eb4e894d949eb0ce8f21368b70f3dd3b",
+    "url": "/static/media/vuesax-login-bg.eb4e894d.jpg"
+  }
+]);
---
+++ build/static/js/8.8ee7253a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[8],{477:function(e,a,t){"use strict";t.r(a);var n=t(14),r=t(15),c=t(16),u=t(17),i=t(0),s=t.n(i),o=function(e){Object(u.a)(t,e);var a=Object(c.a)(t);function t(){return Object(n.a)(this,t),a.apply(this,arguments)}return Object(r.a)(t,[{key:"render",value:function(){return s.a.createElement("h4",null,"This is Page 2.")}}]),t}(s.a.Component);a.default=o}}]);
+//# sourceMappingURL=8.8ee7253a.chunk.js.map
